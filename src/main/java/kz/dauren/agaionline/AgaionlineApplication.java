2115fa40c6ea5261106fd88a2dd3e546c7a346b1
Dauren <dauren.buribekov@gmail.com>
2020-03-28 12:55:21 +0600

    Initial commit

---
+++ src/main/java/kz/dauren/agaionline/AgaionlineApplication.java
@@ -0,0 +1,13 @@
+package kz.dauren.agaionline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgaionlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgaionlineApplication.class, args);
+    }
+
+}
